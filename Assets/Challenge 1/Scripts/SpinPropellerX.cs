93000d9951e22c7dcc4ba02c0c2e0ce98da7c4b3
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2022-09-01 00:30:24 +0100

    Task completed

---
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    private float spinSpeed = 800.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * spinSpeed);
+        // transform.Rotate(0, 0, Time.deltaTime * spinSpeed);
+        
+    }
+}
